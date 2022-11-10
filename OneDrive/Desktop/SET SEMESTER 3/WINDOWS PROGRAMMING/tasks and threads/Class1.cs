5375efa5fa7ae8bf5888946962cdd43f3fee764e
Braychapp <bchapple8815@conestogac.on.ca>
2022-11-09 22:54:12 -0500

    This is about as good of a program as I can get. I have had to remove my live file size checking because I could not figure out how to work with dispatchers within the time I had remaining. Probably could have started the assignment a bit earlier but hopefully I won't lose too many marks for using a message box instead. I think I have all of my file operations in try catch finally blocks. The only one I think I have is when the guids are being written into the file.

---
+++ OneDrive/Desktop/SET SEMESTER 3/WINDOWS PROGRAMMING/tasks and threads/Class1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace tasks_and_threads
+{
+    /*
+   * NAME : FIO
+   * PURPOSE : The FIO class is used for writing to the text file as well as the size monitor task
+   * The class has 3 variables involved with it:
+   * sizeReached: a bool used to tell if the maximum file size has been reached yet or not
+   * writer: writer is used for actually writing to the file within the WriteFile method
+   * mutex: used to queue all the tasks to take turns while working
+   */
+    internal class FIO
+    {
+        //this clas is going to be for writing to the file that is chosen by the user
+
+
+        public volatile bool sizeReached = false;
+        private StreamWriter writer = null;
+        Mutex mutex = null;
+
+        /*
+        * FIO Default Constructor
+        * Description: The default constructor for the FIO class, it gets the mutex ready to be used
+        * Parameters: none
+        * Returns: nothing
+        */
+        public FIO()
+        {
+            //if the mutex is somehow being used already it will release it
+            if (!Mutex.TryOpenExisting("MyMutex", out mutex))
+            {
+                mutex = new Mutex(true, "MyMutex");
+                mutex.ReleaseMutex();
+            }
+        }
+        /*
+       * Method: WriteFile
+       * Description: This method uses all 25 tasks in the array and uses them to write guids to a file
+       * Parameters:string FilePath
+       * Returns: nothing
+       */
+        internal void WriteFile(string FilePath)
+        {
+            while (!sizeReached)
+            {
+                mutex.WaitOne();
+                try
+                {
+                    using (writer = new StreamWriter(FilePath, true))
+                    {
+                        writer.WriteLine(Guid.NewGuid());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Exception: " + ex.Message);
+                }
+                finally
+                {
+                    if (writer != null)
+                    {
+                         writer.Close();
+                    }
+                }
+                mutex.ReleaseMutex();
+            }
+        }
+
+        /*
+       * Method: SizeMonitor
+       * Description: This method is used to keep track of how big the file is through the monitor task
+       * Parameters:string FPath, int MaxSize, Task[] tasks
+       * Returns: nothing
+       */
+        internal void SizeMonitor(string FPath, int MaxSize, Task[] tasks)
+        {
+            FileInfo fi = null;
+            //creating a timer to go off every 0.5 seconds
+            fi = new FileInfo(FPath);
+            Stopwatch timer = new Stopwatch();
+
+            while (!sizeReached)
+            {
+                //starting the 500 ms timer
+                timer.Start();
+                if (timer.ElapsedMilliseconds >= 500)
+                {
+                    sizeReached = CheckSize(FPath, MaxSize);
+                    //i wanted to have a text box in my window that would automatically update to meet the file size but I spent 
+                    //literally hours trying to do that and I couldn't so i am sorry but this is the only way I could get it to go :(
+                    //sorry for message box spam
+                    
+                    timer.Restart();
+                }
+
+                if (sizeReached)
+                {
+                    foreach (Task task in tasks)
+                    {
+                        task.Wait();
+                    }
+                }
+                Thread.Sleep(1);
+                
+            }
+            fi = new FileInfo(FPath);
+            MessageBox.Show("DONE!\nFinal File Size: " + fi.Length.ToString() + " bytes");
+        }
+
+        /*
+       * Method: CheckSize
+       * Description: This method is used for directly checking if the file's size is too big, and if it's not then the program will continue
+       * Parameters:string FPath, int MaxSize
+       * Returns: nothing
+       */
+        internal bool CheckSize(string FPath, int MaxSize)
+        {
+            
+            FileInfo fi = null;
+            fi = new FileInfo(FPath);
+
+            if (fi.Length >= MaxSize)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Current File Size: " + fi.Length.ToString() + " bytes");
+                return false;
+            }
+            
+        }
+
+    }
+
+
+
+
+
+
+
+    
+}
---
+++ OneDrive/Desktop/SET SEMESTER 3/WINDOWS PROGRAMMING/tasks and threads/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿/*
+* FILE : MainWindow.xaml.cs
+* PROJECT : A04 tasks and threads
+* PROGRAMMER : Brayden Chapple
+* FIRST VERSION : 2022-11-09
+* DESCRIPTION : This program writes guids into a text file up to the specified size by the user and lets them know the size of the file as it runs
+* through message boxes
+*/
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tasks_and_threads
+{
+    /*
+    * NAME : MainWindow
+    * PURPOSE : The MainWindow class is for displaying the main window to the user and allowing them to interact
+    * with the features associated with the main window
+    * The class has 4 variables involved with it:
+    * tasks: an array of tasks for use in writing the file full of guids
+    * Monitor: a lone task used to monitor the size of the file every 0.5 seconds or so
+    * file: an object of type FIO which is used for altering the text file
+    * taskNumbers: used to tell the program how many tasks to use in the array
+    */
+    public partial class MainWindow : Window
+    {
+        //creating tasks
+        Task[] tasks;
+        Task Monitor;
+        FIO file = new FIO();
+        int taskNumbers = 25;
+
+        /*
+        * MainWindow Default Constructor
+        * Description: The default constructor for the MainWindow class, it does not assign any variables
+        * Parameters: none
+        * Returns: nothing
+        */
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /*
+       * Method: FileIo_Click
+       * Description: This method is used to open up a text file of the user's choosing
+       * Parameters:object sender, RoutedEventArgs e
+       * Returns: nothing
+       */
+        private void FileIO_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            {
+                Title = "Open File";
+            }
+            saveFileDialog.Filter = "Text files (.*txt)|*.txt|All files(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                //needs to change the filepath and be able to transport that variable elsewhere
+                var path = string.Empty;
+                path = saveFileDialog.FileName;
+                FilePath.Text = path;
+
+
+            }
+        }
+
+        /*
+       * Method: TextBox_TextChanged
+       * Description: This method does nothing
+       * Parameters:object sender, TextChangedEventArgs e
+       * Returns: nothing
+       */
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        /*
+       * Method: GenerateFile_Click
+       * Description: This method is used for input validation, as well as turning on all the tasks to start writing to the file
+       * Parameters:object sender, RoutedEventArgs e
+       * Returns: nothing
+       */
+        private void GenerateFile_Click(object sender, RoutedEventArgs e)
+        {
+            //validate that a file was selected before doing anything
+            string fPath = FilePath.Text;
+            if (fPath == "File Path:")
+            {
+               MessageBox.Show("Please select a file before attempting to generate file", "File error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            //next validation is the file's target size
+            int Target = Convert.ToInt32(FileSize.Text);
+            if (1000 > Target)
+            {
+                MessageBox.Show("Please input a size between 1,000 and 20,000,000", "Size error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if(Target >= 20000000)
+            {
+                MessageBox.Show("Please input a size between 1,000 and 20,000,000", "Size error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            //make sure the file is empty at this point 
+            using (var fs = new FileStream(fPath, FileMode.Truncate))
+            {
+                fs.Close();
+            }
+
+                tasks = new Task[taskNumbers];
+
+            file.sizeReached = false;
+            Monitor = new Task(() => file.SizeMonitor(fPath, Target, tasks));
+            Monitor.Start();
+            for(int i = 0; i < taskNumbers; i++)
+            {
+                tasks[i] = new Task(() => file.WriteFile(fPath));
+                tasks[i].Start();
+            }
+            
+            
+            
+
+        }
+
+        
+    }
+}
